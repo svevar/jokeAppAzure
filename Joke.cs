8038d07980a14d8ae7eb9bfe08d647f97b74fda9
svevar <svennat@gmail.com>
2020-09-28 07:29:27 +0200

    Validate joke from API and insert into SQL Database using Dapper and System.Data.SqlClient

--- Joke.cs
+++ Joke.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace JokeApp
@@ -16,5 +17,24 @@ namespace JokeApp
         public string Setup { get => setup; set => setup = value; }
         public string Punchline { get => punchline; set => punchline = value; }
         
+        public Boolean ValidateJoke()
+        {
+            // Initial validation true indicating a pass
+            // We make a straight forward if-else check to validate different lenghts
+            // From Simen: "You implement this function as you please. ...
+            // I've chosen to do it simple, explicit and un-elegant for the sake of teaching"
+            var validation = true;
+
+            if (this.Id < 0)  // Validate non-negative id
+            {
+                validation = false;
+            } else if (this.Type.Length > 255 
+                    || this.Setup.Length > 255 
+                    || this.Punchline.Length > 255) // Validate length doesn't exceed the 255 char limit in our db
+            {
+                validation = false;
+            }
+            return validation;
+        }
     }
 }
--- Main.cs
+++ Main.cs
@@ -2,6 +2,8 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace JokeApp
 {
@@ -15,10 +17,44 @@ namespace JokeApp
             log.LogInformation($"API URL is: {JokeAPI.APIURL}");
 
             // Kjør metoden for å gjøre spørring til APIet
-            var joke = JokeAPI.GETRandomJoke();
+            //var joke = new Joke();
+            var joke = new Joke();
+            Boolean acceptedJoke = false;
+            int retries = 0;
+            int maxRetry = 10;
+            while (!acceptedJoke && retries < maxRetry)
+            {
+                // Kjør metoden for å gjøre spørring til APIet
+                // JokeAPI.GETRandomJoke();
+                joke = JokeAPI.GETRandomJoke();
+                acceptedJoke = joke.ValidateJoke();
+                retries += 1;
+            }
+            //if (!acceptedJoke)
+            //{
+            //    // Hvis vi bruker mer enn maxRetry forsøk på å finne en godkjent vits avslutter vi kjøringen
+            //    log.LogInformation($"After {maxRetry} subsequent tries, function couldn't retrieve a joke that passed validation.");
+            //    return;
+            //}
+
+            // Logger ut vitsen til konsoll:
             log.LogInformation($"Her kommer en vits!");
             log.LogInformation($"{joke.Setup}");
             log.LogInformation($"{joke.Punchline}");
+
+            // Vi henter connectionString til databasen fra miljøvariabler
+            string ConnectionString = System.Environment.GetEnvironmentVariable("SQLConnectionString", EnvironmentVariableTarget.Process);
+            //log.LogInformation($"Vår connection string: {ConnectionString}");
+
+            // Oppretter tilkobling til databasen vha. System.Data.SqlClient og åpner denne
+            using (var con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+
+            // Definerer spørringen vår og kjører denne
+            con.Execute("insert into Jokes2 (Id, JokeType, Setup, Punchline) values (@Id, @Type, @Setup, @Punchline)",
+                joke);
+            }
         }
     }
 }
